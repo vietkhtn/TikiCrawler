75cbdffaa95c3853f3e9ac19812cdf8486b0264f
vietkhtn <nohackerjustdev@gmail.com>
2020-12-25 09:29:51 +0700

    Seperate 2 files ProductInfo & UserRating

--- TikiCrawler/TikiCrawler/spiders/TikiProductInfo.py
+++ TikiCrawler/TikiCrawler/spiders/TikiProductInfo.py
@@ -1,6 +1,6 @@
 import scrapy
 import re
-from ..items import TikiProductInfo, TikiUserRating
+from ..items import TikiProductInfo
 
 
 class ProductItem(scrapy.Spider):
@@ -45,44 +45,6 @@ class ProductItem(scrapy.Spider):
         item['productCategory'] = product_category
         yield item
 
-class UserRating(scrapy.Spider):
-    name = "TikiUserRating"
-    start_urls = ["https://tiki.vn/dien-gia-dung/c1882?src=c.1882.hamburger_menu_fly_out_banner"]
 
-    def parse(self, response):
-        listProduct = response.css(".product-item")
-
-        for product in listProduct:
-            # Lấy productID từ đường Link SP
-            productLink = product.css("a::attr(href)").extract()
-            temp = re.search('\d{6,8}', str(productLink))
-            productID = temp.group()
-
-            # Gọi tới API chứa thông tin sp lấy 50 comment
-            url = f'https://tiki.vn/api/v2/reviews?product_id={productID}&sort=score|desc,id|desc,stars|all&page=1&limit=100'
-            yield response.follow(url=url, callback=self.parseUserRating)
-
-    def parseUserRating(self, response):
-        userRating = TikiUserRating()
-        userRateList = re.findall('(?<="full_name":").*?(?=",)', response.text)
-        productRate = re.search('(?<="product_id":).*?(?=},)', response.text)
-        timeRateList = re.findall('(?<="created_at":).*?(?=,)', response.text)
-        startRateList = re.findall('(?<="rating":).*?(?=,)', response.text)
-        commentRateList = re.findall('(?<="content":").*?(?=",")', response.text)
-        for i in range(0, len(userRateList)):
-            userRate = userRateList[i]
-            timeRate = timeRateList[i]
-            starRate = startRateList[i]
-            commentRate = commentRateList[i]
-            print(len(commentRate))
-            if len(commentRate) > 0:
-                if commentRate != '","status":"approved':
-                    userRating['stt'] = i
-                    userRating['userRate'] = userRate
-                    userRating['productRate'] = productRate.group()
-                    userRating['timeRate'] = timeRate
-                    userRating['starRate'] = starRate
-                    userRating['commentRate'] = commentRate
-                    yield userRating
 
 
--- TikiCrawler/TikiCrawler/spiders/TikiUserRating.py
+++ TikiCrawler/TikiCrawler/spiders/TikiUserRating.py
@@ -1,9 +1,9 @@
 import scrapy
 import re
-from ..items import TikiProductInfo, TikiUserRating
+from ..items import TikiUserRating
 
 class UserRating(scrapy.Spider):
-    name = "TikiUserRating123"
+    name = "TikiUserRating"
     start_urls = ["https://tiki.vn/dien-gia-dung/c1882?src=c.1882.hamburger_menu_fly_out_banner"]
 
     def parse(self, response):
@@ -15,9 +15,8 @@ class UserRating(scrapy.Spider):
             temp = re.search('\d{6,8}', str(productLink))
             productID = temp.group()
 
-
             # Gọi tới API chứa thông tin sp lấy 50 comment
-            url = f'https://tiki.vn/api/v2/reviews?product_id=31502009&sort=score|desc,id|desc,stars|all&page=1&limit=40'
+            url = f'https://tiki.vn/api/v2/reviews?product_id={productID}&sort=score|desc,id|desc,stars|all&page=1&limit=100'
             yield response.follow(url=url, callback=self.parseUserRating)
 
     def parseUserRating(self, response):
@@ -34,10 +33,11 @@ class UserRating(scrapy.Spider):
             commentRate = commentRateList[i]
             print(len(commentRate))
             if len(commentRate) > 0:
-                userRating['stt'] = i
-                userRating['userRate'] = userRate
-                userRating['productRate'] = productRate.group()
-                userRating['timeRate'] = timeRate
-                userRating['starRate'] = starRate
-                userRating['commentRate'] = commentRate
-                yield userRating
+                if commentRate != '","status":"approved':
+                    userRating['stt'] = i
+                    userRating['userRate'] = userRate
+                    userRating['productRate'] = productRate.group()
+                    userRating['timeRate'] = timeRate
+                    userRating['starRate'] = starRate
+                    userRating['commentRate'] = commentRate
+                    yield userRating
